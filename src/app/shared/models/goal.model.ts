c80bfc4430fe5392046c2b414ef155f74db2cedd
Reed Martz <reed@reedmartz.com>
2018-10-07 16:47:32 -0400

    Add basic styling

--- src/app/shared/models/goal.model.ts
+++ src/app/shared/models/goal.model.ts
@@ -1,13 +1,39 @@
 export class Goal {
   constructor() {
     this.current = 0;
+    this.purchased = false;
   }
 
   label: string;
   target: number;
   current: number;
+  purchased: boolean;
+
+  public isFunded(): boolean {
+    return this.current >= this.target;
+  }
 
   public isActive(): boolean {
-    return this.current < this.target;
+    // A goal is active if it hasn't met its target yet
+    return !this.isFunded();
+  }
+
+  public isComplete(): boolean {
+    // A goal is complete if it has been fully funded but not yet purchased.
+    return !this.purchased && this.isFunded();
+  }
+
+  public isHidden(): boolean {
+    // Goals are hidden once they are purchased and fully funded
+    return this.purchased && this.isFunded();
+  }
+
+  public isVisible(): boolean {
+    return !this.isHidden();
+  }
+
+  public isOverdrawn(): boolean {
+    // A goal is overdrawn if it was purchased but still has yet to reach its goal.
+    return this.purchased && !this.isFunded();
   }
 }

264e61730a3359094dd0baf97c440933cbd6e2f1
Reed Martz <reed@reedmartz.com>
2018-10-07 09:22:46 -0400

    Rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to itemized-saving!');
+    expect(page.getParagraphText()).toEqual('Welcome to saving-goals!');
   });
 });

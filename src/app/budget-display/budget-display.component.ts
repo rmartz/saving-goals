66a310e462c4b534e1e676cbd08bb6d15fabcc7b
Reed Martz <reed@reedmartz.com>
2018-10-24 19:08:33 -0400

    Allow renaming budgets

--- src/app/budget-display/budget-display.component.ts
+++ src/app/budget-display/budget-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Budget } from '../shared/models/budget.model';
 import { Budgets } from '../shared/services/budgets.service';
 
@@ -6,10 +6,13 @@ import { Budgets } from '../shared/services/budgets.service';
   selector: 'app-budget-display',
   templateUrl: './budget-display.component.html'
 })
-export class BudgetDisplayComponent {
+export class BudgetDisplayComponent implements OnInit {
 
   @Input()
   public budget: Budget;
+
+  public renameMode = false;
+  public budgetLabel: string;
 
   constructor(private budgets: Budgets) { }
 
@@ -17,5 +20,15 @@ export class BudgetDisplayComponent {
     const amountCents = Math.round(amount * 100);
     this.budget.disperse(amountCents);
     this.budgets.save(this.budget);
+  }
+
+  public rename() {
+    this.budget.label = this.budgetLabel;
+    this.budgets.save(this.budget);
+    this.renameMode = false;
+  }
+
+  public ngOnInit() {
+    this.budgetLabel = this.budget.label;
   }
 }

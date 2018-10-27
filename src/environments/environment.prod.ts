af383a8f2119995cdedcc1a9bb1c573769a706a4
Reed Martz <reed@reedmartz.com>
2018-10-27 17:07:17 -0400

    Set Firebase config in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAgsd6oe0G8MnI5Dj7iCiKGit5aIacbSAc',
+    authDomain: 'saving-goals.firebaseapp.com',
+    databaseURL: 'https://saving-goals.firebaseio.com',
+    projectId: 'saving-goals',
+    storageBucket: 'saving-goals.appspot.com',
+    messagingSenderId: '601029165790'
+  }
 };

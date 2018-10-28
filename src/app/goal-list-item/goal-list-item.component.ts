2f57cc9f9d08ba0e2ff1045a19a01a911247a0ee
Reed Martz <reed@reedmartz.com>
2018-10-28 16:12:59 -0400

    Refactor goal item mode logic

--- src/app/goal-list-item/goal-list-item.component.ts
+++ src/app/goal-list-item/goal-list-item.component.ts
@@ -2,6 +2,13 @@ import { Component, Input } from '@angular/core';
 import { Goal } from '../shared/models/goal.model';
 import { Budgets } from '../shared/services/budgets.service';
 
+enum GoalListItemMode {
+  Default,
+  Purchase,
+  Archive,
+  Edit
+}
+
 @Component({
   selector: 'app-goal-list-item',
   templateUrl: './goal-list-item.component.html'
@@ -10,8 +17,7 @@ export class GoalListItemComponent {
   @Input()
   public goal: Goal;
 
-  public editMode: boolean;
-  public purchaseMode: boolean;
+  public mode: GoalListItemMode;
 
   constructor(public budgets: Budgets) { }
 
@@ -23,5 +29,46 @@ export class GoalListItemComponent {
   public moveDown() {
     this.goal.budget.moveDown(this.goal);
     this.budgets.save(this.goal.budget);
+  }
+
+  public archive() {
+    this.goal.budget.archive(this.goal);
+    this.budgets.save(this.goal.budget);
+  }
+
+  public resetMode() {
+    this.mode = GoalListItemMode.Default;
+  }
+
+  public checkMode(mode: GoalListItemMode): boolean {
+    return this.mode === mode;
+  }
+
+  public toggleMode(mode: GoalListItemMode) {
+    this.mode = this.checkMode(mode) ? GoalListItemMode.Default : mode;
+  }
+
+  public purchaseMode(): boolean {
+    return this.checkMode(GoalListItemMode.Purchase);
+  }
+
+  public togglePurchaseMode() {
+    this.toggleMode(GoalListItemMode.Purchase);
+  }
+
+  public editMode(): boolean {
+    return this.checkMode(GoalListItemMode.Edit);
+  }
+
+  public toggleEditMode() {
+    this.toggleMode(GoalListItemMode.Edit);
+  }
+
+  public archiveMode(): boolean {
+    return this.checkMode(GoalListItemMode.Archive);
+  }
+
+  public toggleArchiveMode() {
+    this.toggleMode(GoalListItemMode.Archive);
   }
 }

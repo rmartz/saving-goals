79698ced88e4153167fb62280915a6614921cac8
Reed Martz <reed@reedmartz.com>
2018-10-28 12:52:13 -0400

    Provide more detailed warning for purchasing a goal 'on credit'

--- src/app/purchase-goal-item/purchase-goal-item.component.ts
+++ src/app/purchase-goal-item/purchase-goal-item.component.ts
@@ -29,4 +29,20 @@ export class PurchaseGoalItemComponent implements OnInit {
     this.budgets.save(this.goal.budget);
     this.close.emit(this.goal);
   }
+
+  public exceedTotalBalance(): boolean {
+    // Return True if cost is greater than the total amount saved across all goals
+    return this.cost * 100 > this.goal.budget.totalBalance();
+  }
+
+  public exceedAccrued(): boolean {
+    // Return True if cost is greater than amount saved
+    return this.cost * 100 > this.goal.current;
+  }
+
+  public impedeGoal(): boolean {
+    // Return True if purchasing this goal for cost would cause total saved balance to be below the total allocated to another goal
+    const estimatedBalance = this.goal.budget.totalBalance() - this.cost * 100;
+    return this.goal.budget.goals.some(goal => goal !== this.goal && goal.current > estimatedBalance);
+  }
 }

be8dfa74d56617b18bea7fb0a9d144df49a48b56
Reed Martz <reed@reedmartz.com>
2021-01-18 19:55:05 -0500

    Refactor dispersement to use explicit interface instead of structured array format

--- src/app/shared/utils/dispersement.ts
+++ src/app/shared/utils/dispersement.ts
@@ -1,11 +1,29 @@
+import { NumberValueAccessor } from '@angular/forms/src/directives';
 import { Budget } from '../models/budget.model';
 import { Goal } from '../models/goal.model';
 import { roundRandom } from './round';
 
-function weightGoals(goals: Goal[]): [Goal, number][] {
-  const weightedGoals = goals.map<[Goal, number]>((goal, index) => [goal, 1.0 / (index + 1)]);
-  const sum = weightedGoals.reduce((a, b) => a + b[1], 0);
-  return weightedGoals.map<[Goal, number]>(value => [value[0], value[1] / sum]);
+interface WeightedGoal {
+  goal: Goal;
+  weight: number;
+}
+
+
+function weightGoals(goals: Goal[]): WeightedGoal[] {
+  const weightedGoals = goals.map<WeightedGoal>((goal, index) => ({
+    goal,
+    weight: 1.0 / (index + 1),
+  }));
+
+  const totalWeight = weightedGoals.reduce((sum, w) => sum + w.weight, 0);
+
+  // Scale all the weights by the sum so their values add to 1
+  // This lets us multiply the amount to disperse by the weight to get this goal's proportion
+  weightedGoals.forEach(w => {
+    w.weight /= totalWeight;
+  });
+
+  return weightedGoals;
 }
 
 export function disperseBalance(budget: Budget, total: number): void {
@@ -13,6 +31,7 @@ export function disperseBalance(budget: Budget, total: number): void {
     // No value to contribute
     return;
   }
+
   const activeGoals = budget.goals.filter(goal => goal.isActive());
   if (activeGoals.length === 0) {
     // There are no unfunded goals to contribute to
@@ -22,13 +41,15 @@ export function disperseBalance(budget: Budget, total: number): void {
   const goalWeights = weightGoals(activeGoals);
 
   let remainder = total;
-  for (const [goal, weight] of goalWeights) {
+  for (const {weight, goal} of goalWeights) {
     // Figure how much our weight says this goal should receive
-    // We want to use whole numbers to avoid floating point error.
+
+    // We want to use whole numbers to avoid floating point errors...
     // Math.floor could cause an infinite loop with 1 cent never distributed.
     // Math.ceil causes a one-sided rounding error that means small contributions might always get used up before they reach lower goals.
     // roundRandom probabilistically rounds up or down, so it should minimize risk of always starving lower goals.
     const target = roundRandom(total * weight);
+
     // Do not give more to the goal than the goal actually wants, or what we have left
     const value = Math.min(target, remainder, goal.target - goal.current);
     remainder -= value;
